d23e7c8c2ffb70e00e8017b0da48704e6f061961
xra <hey@im-xra.dev>
2025-06-04 15:56:33 +0100

    removed testing script

--- src/modules/endpoints/t-line/t-line.controller.ts
+++ src/modules/endpoints/t-line/t-line.controller.ts
@@ -1,22 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { RedisCacheDriverService } from '../../redis-cache-driver/redis-cache-driver.service';
 
 @Controller('t-line')
 export class TLineController {
-    constructor(private readonly cacheService: RedisCacheDriverService) {}
 
     @Get()
     async ping() {
-        const client = await this.cacheService.getClient();
-        const builder = client.multi();
-
-        builder.lRange('list1', 0, -1);
-        builder.del('list1');
-        builder.lRange('list2', 0, -1);
-        builder.del('list2');
-
-        const out = await builder.exec();
-
-        console.log(out);
     }
 }
